e8c9217820cb51b52352dabf415f5bbd3c25829f
Praneet9 <praneetbomma@gmail.com>
2020-08-31 13:59:31 +0530

    node arm version, boot screen location change

--- deploy/scripts/boot_screen.sh
+++ deploy/scripts/boot_screen.sh
@@ -3,6 +3,6 @@
 echo "********** Setting up custom boot screen **********"
 
 cd ~/pufferfish-vent-software/deploy
-sudo mkdir -p /etc/plymouth/themes
+sudo mkdir -p /usr/share/plymouth/themes
 sudo cp plymouth/plymouthd.conf /etc/plymouth/plymouthd.conf
-sudo cp -r plymouth/themes/pufferfish /etc/plymouth/themes/
+sudo cp -r plymouth/themes/pufferfish /usr/share/plymouth/themes/
