3f4998f77e72a3190e5a85c2704dda47f5810dbe
Ethan Li <lietk12@gmail.com>
2020-08-30 21:19:07 -0700

    Merge all files into a single clang-tidy command

--- firmware/ventilator-controller-stm32/clang-tidy-all.sh
+++ firmware/ventilator-controller-stm32/clang-tidy-all.sh
@@ -5,48 +5,35 @@ ERROR=0  # exit code
 COMPILE_COMMANDS_DATABASE="-p cmake-build-debug"
 STANDARD_ARGS="$COMPILE_COMMANDS_DATABASE $@"
 
-# Set file filters for most files
+# Set file filters
 IGNORE_FILES="stm32h7xx_hal_conf.h"
+STM32_USER_FILES="Core/Inc/main.h \
+  Core/Src/main.cpp \
+  Core/Inc/stm32h7xx_it.h \
+  Core/Src/stm32h7xx_hal_msp.c \
+  Core/Src/stm32h7xx_it.cpp"
 INCLUDE_EXTENSIONS=".h .cpp .tpp"
+
+# Set line filters based on file filters
 EXCLUDE_FILTERS=`echo $IGNORE_FILES | tr ' ' '\n' \
   | awk '{print $1 " [1,1]"}' \
   | ./clang-tidy-line-filters.sh`
 INCLUDE_FILTERS=`echo $INCLUDE_EXTENSIONS | tr ' ' '\n' \
   | ./clang-tidy-line-filters.sh`
-LINE_FILTERS="[$EXCLUDE_FILTERS,$INCLUDE_FILTERS]"
-
-# Check source (and included header) files
-FILES=`find Core/Src/Pufferfish -iname *.cpp | xargs`
-echo clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS
-clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS || ERROR=1
-echo
-
-# # Check test source (and included header) files
-# FILES=`find Core/Test -iname *.cpp | xargs`
-# echo clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS
-# clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS || ERROR=1
-# echo
-
-# Check STM32CubeMX auto-generated files
-MX_GENERATED="Core/Inc/main.h \
-  Core/Src/main.cpp \
-  Core/Inc/stm32h7xx_it.h \
-  Core/Src/stm32h7xx_hal_msp.c \
-  Core/Src/stm32h7xx_it.cpp"
-LINE_FILTERS=$(
-  for FILE in $MX_GENERATED; do
+STM32_USER_FILTERS=$(
+  for FILE in $STM32_USER_FILES; do
     LINES=`./grep-stm32cubemx-user.sh "$FILE" \
       | awk 'BEGIN {FS=":"}; {print "[" $1 "," $2 "]" }' \
       | paste -s -d, -`
     echo "$FILE $LINES"
   done
 )
-LINE_FILTERS=`echo "$LINE_FILTERS" | ./clang-tidy-line-filters.sh`
-LINE_FILTERS="[$LINE_FILTERS]"
-for FILE in $MX_GENERATED; do
-  echo clang-tidy "$FILE" --line-filter="$LINE_FILTERS" $STANDARD_ARGS
-  clang-tidy "$FILE" --line-filter="$LINE_FILTERS" $STANDARD_ARGS || ERROR=1
-  echo
-done
+STM32_USER_FILTERS=`echo "$STM32_USER_FILTERS" | ./clang-tidy-line-filters.sh`
+echo $STM32_USER_FILTERS
+LINE_FILTERS="[$EXCLUDE_FILTERS,$STM32_USER_FILTERS,$INCLUDE_FILTERS]"
 
-exit $ERROR
+# Check files
+SOURCE_FILES=`find Core/Src/Pufferfish -iname *.cpp | xargs`
+FILES="$SOURCE_FILES $STM32_USER_FILES"
+echo clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS
+clang-tidy $FILES --line-filter="$LINE_FILTERS" $STANDARD_ARGS
