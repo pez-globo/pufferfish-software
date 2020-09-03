803c8f24dccd94cb52ec9e7e777235a3f2f519d0
Ethan Li <lietk12@gmail.com>
2020-09-02 22:51:43 -0700

    Add copyright/license notice for the work used to create RingBuffer

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Util/RingBuffer.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Util/RingBuffer.h
@@ -1,10 +1,49 @@
-/*
- * Copyright 2020, the Pez Globo team and the Pufferfish project contributors
- *
- *      Author: Ethan Li
- *
- *  An interrupt-safe byte stream backed by a circular buffer.
- */
+/// \file
+/// \brief An interrupt-safe byte stream backed by a circular buffer.
+
+// Copyright (c) 2020 Pez-Globo and the Pufferfish project contributors
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is an adaptation of code published at
+// https://github.com/hexagon5un/embed_with_elliot-circular_buffer
+// by Elliot Williams under the MIT License. This implementation makes
+// significant implementations to provide a C++ interface.
+// Third-party works included this file are licensed as follows:
+//
+// Copyright (c) 2015 Elliot Williams
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 
 #pragma once
 
@@ -17,7 +56,7 @@ namespace Util {
 
 
 /**
- * Circular buffer with non-blocking queue interface.
+ * Byte stream with non-blocking queue interface and static allocation.
  *
  * Inspired by https://hackaday.com/2015/10/29/embed-with-elliot-going-round-with-circular-buffers/
  * This class provides a bounded-length queue data structure which is
