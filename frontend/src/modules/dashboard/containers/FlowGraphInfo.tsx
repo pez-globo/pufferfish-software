b27bef06848287ff16f55f30f6c00381f320215f
Sudhir Kumar <sudhirkumark@outlook.in>
2020-09-12 00:18:42 +0530

    Fixes for eslint/prettier issues

--- frontend/src/modules/dashboard/containers/FlowGraphInfo.tsx
+++ frontend/src/modules/dashboard/containers/FlowGraphInfo.tsx
@@ -1,67 +1,70 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import { AutoSizer } from 'react-virtualized'
-import { StoreState } from '../../../store/types'
-import { WaveformPoint } from '../../../store/controller/types'
-import {
-  getWaveformFlowOld,
-  getWaveformFlowNew
-} from '../../../store/controller/selectors'
-import { Waveform } from '../components/Waveform'
-import { Axes } from '../components/Axes'
-
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { AutoSizer } from 'react-virtualized';
+import { StoreState } from '../../../store/types';
+import { WaveformPoint } from '../../../store/controller/types';
+import { getWaveformFlowOld, getWaveformFlowNew } from '../../../store/controller/selectors';
+import { Waveform } from '../components/Waveform';
+import { Axes } from '../components/Axes';
 
 interface DataProps {
-  data: WaveformPoint[]
+  data: WaveformPoint[];
 }
-
 
 const oldSelector = createStructuredSelector<StoreState, DataProps>({
   data: getWaveformFlowOld,
-})
+});
 const newSelector = createStructuredSelector<StoreState, DataProps>({
   data: getWaveformFlowNew,
-})
-
+});
 
 interface AutoSizerProps {
-  width: number,
-  height: number
+  width: number;
+  height: number;
 }
-
 
 interface WaveformProps extends DataProps, AutoSizerProps {
-  strokeWidth: number,
-  fill: boolean
+  strokeWidth: number;
+  fill: boolean;
 }
 
-
-const FlowWaveform = ({ data, width, height, strokeWidth, fill }: WaveformProps) => (
+const FlowWaveform = ({ data, width, height, strokeWidth, fill }: WaveformProps): JSX.Element => (
   <Waveform
-    type={'flow'}
+    type="flow"
     fill={fill}
-    width={width} height={height} data={data} strokeWidth={strokeWidth}
-    xRangeMax={10000} yRangeMin={-150} yRangeMax={150}
+    width={width}
+    height={height}
+    data={data}
+    strokeWidth={strokeWidth}
+    xRangeMax={10000}
+    yRangeMin={-150}
+    yRangeMax={150}
   />
-)
+);
 
+const WaveformOld = connect(oldSelector)(FlowWaveform);
+const WaveformNew = connect(newSelector)(FlowWaveform);
 
-const WaveformOld = connect(oldSelector)(FlowWaveform)
-const WaveformNew = connect(newSelector)(FlowWaveform)
-
-const FlowGraphInfo = () => (
+const FlowGraphInfo = (): JSX.Element => (
   <AutoSizer>
-    {({ width, height }: AutoSizerProps) => (
-      <Axes
-        width={width} height={height}
-        waveformOld={<WaveformOld width={width} height={height} strokeWidth={1} fill={false}/>}
-        waveformNew={<WaveformNew width={width} height={height} strokeWidth={4} fill={true}/>}
-        xRangeMax={10000} yRangeMin={-150} yRangeMax={150}
-        title={"Flow"} units={"L/min"}
-      />
-    )}
+    {(props: AutoSizerProps) => {
+      const { width, height } = props;
+      return (
+        <Axes
+          width={width}
+          height={height}
+          waveformOld={<WaveformOld width={width} height={height} strokeWidth={1} fill={false} />}
+          waveformNew={<WaveformNew width={width} height={height} strokeWidth={4} fill={true} />}
+          xRangeMax={10000}
+          yRangeMin={-150}
+          yRangeMax={150}
+          title="Flow"
+          units="L/min"
+        />
+      );
+    }}
   </AutoSizer>
-)
+);
 
-export default FlowGraphInfo
+export default FlowGraphInfo;
--- frontend/src/modules/dashboard/containers/PawGraphInfo.tsx
+++ frontend/src/modules/dashboard/containers/PawGraphInfo.tsx
@@ -1,67 +1,70 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { createStructuredSelector } from 'reselect'
-import { AutoSizer } from 'react-virtualized'
-import { StoreState } from '../../../store/types'
-import { WaveformPoint } from '../../../store/controller/types'
-import {
-  getWaveformPawOld,
-  getWaveformPawNew
-} from '../../../store/controller/selectors'
-import { Waveform } from '../components/Waveform'
-import { Axes } from '../components/Axes'
-
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { AutoSizer } from 'react-virtualized';
+import { StoreState } from '../../../store/types';
+import { WaveformPoint } from '../../../store/controller/types';
+import { getWaveformPawOld, getWaveformPawNew } from '../../../store/controller/selectors';
+import { Waveform } from '../components/Waveform';
+import { Axes } from '../components/Axes';
 
 interface DataProps {
-  data: WaveformPoint[]
+  data: WaveformPoint[];
 }
-
 
 const oldSelector = createStructuredSelector<StoreState, DataProps>({
   data: getWaveformPawOld,
-})
+});
 const newSelector = createStructuredSelector<StoreState, DataProps>({
   data: getWaveformPawNew,
-})
-
+});
 
 interface AutoSizerProps {
-  width: number,
-  height: number
+  width: number;
+  height: number;
 }
-
 
 interface WaveformProps extends DataProps, AutoSizerProps {
-  strokeWidth: number,
-  fill: boolean
+  strokeWidth: number;
+  fill: boolean;
 }
 
-
-const PawWaveform = ({ data, width, height, strokeWidth, fill }: WaveformProps) => (
+const PawWaveform = ({ data, width, height, strokeWidth, fill }: WaveformProps): JSX.Element => (
   <Waveform
-    type={'paw'}
+    type="paw"
     fill={fill}
-    width={width} height={height} data={data} strokeWidth={strokeWidth}
-    xRangeMax={10000} yRangeMin={0} yRangeMax={60}
+    width={width}
+    height={height}
+    data={data}
+    strokeWidth={strokeWidth}
+    xRangeMax={10000}
+    yRangeMin={0}
+    yRangeMax={60}
   />
-)
+);
 
+const WaveformOld = connect(oldSelector)(PawWaveform);
+const WaveformNew = connect(newSelector)(PawWaveform);
 
-const WaveformOld = connect(oldSelector)(PawWaveform)
-const WaveformNew = connect(newSelector)(PawWaveform)
-
-const PawGraphInfo = () => (
+const PawGraphInfo = (): JSX.Element => (
   <AutoSizer>
-    {({ width, height }: AutoSizerProps) => (
-      <Axes
-        width={width} height={height}
-        waveformOld={<WaveformOld width={width} height={height} strokeWidth={1} fill={false}/>}
-        waveformNew={<WaveformNew width={width} height={height} strokeWidth={4} fill={true}/>}
-        xRangeMax={10000} yRangeMin={0} yRangeMax={60}
-        title={"Paw"} units={"cm H2O"}
-      />
-    )}
+    {(props: AutoSizerProps) => {
+      const { width, height } = props;
+      return (
+        <Axes
+          width={width}
+          height={height}
+          waveformOld={<WaveformOld width={width} height={height} strokeWidth={1} fill={false} />}
+          waveformNew={<WaveformNew width={width} height={height} strokeWidth={4} fill={true} />}
+          xRangeMax={10000}
+          yRangeMin={0}
+          yRangeMax={60}
+          title="Paw"
+          units="cm H2O"
+        />
+      );
+    }}
   </AutoSizer>
-)
+);
 
-export default PawGraphInfo
+export default PawGraphInfo;
