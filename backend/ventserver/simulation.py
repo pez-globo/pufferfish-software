4b89c2db394a2275c8335fa157a994af0a6f420c
Ethan Li <lietk12@gmail.com>
2020-09-21 14:20:46 -0700

    Decompose backend simulator

--- backend/ventserver/simulation.py
+++ backend/ventserver/simulation.py
@@ -5,6 +5,8 @@ import time
 import typing
 from typing import Mapping, Optional, Type
 
+import attr
+
 import trio
 
 from ventserver.integration import _trio
@@ -15,99 +17,191 @@ from ventserver.protocols import server
 from ventserver.protocols.protobuf import mcu_pb
 
 
+@attr.s
+class BreathingCircuitSimulator:
+    """Base class for a breathing circuit simulator."""
+
+    SENSOR_UPDATE_INTERVAL = 2
+
+    all_states: Mapping[
+        Type[application.PBMessage], Optional[application.PBMessage]
+    ] = attr.ib()
+    current_time: float = attr.ib(default=0)  # ms after initial_time
+    previous_time: float = attr.ib(default=0)  # ms after initial_time
+    initial_time: float = attr.ib(default=time.time() * 1000)  # ms, Unix time
+
+    # FiO2
+    fio2_responsiveness: float = attr.ib(default=0.01)  # ms
+    fio2_noise: float = attr.ib(default=0.005)  # % FiO2
+
+    # State segments
+
+    @property
+    def _sensor_measurements(self) -> mcu_pb.SensorMeasurements:
+        """Access the SensorMeasurements state segment."""
+        return typing.cast(
+            mcu_pb.SensorMeasurements,
+            self.all_states[mcu_pb.SensorMeasurements]
+        )
+
+    @property
+    def _cycle_measurements(self) -> mcu_pb.CycleMeasurements:
+        """Access the CycleMeasurements state segment."""
+        return typing.cast(
+            mcu_pb.CycleMeasurements, self.all_states[mcu_pb.CycleMeasurements]
+        )
+
+    @property
+    def _parameters_request(self) -> mcu_pb.ParametersRequest:
+        """Access the ParametersRequest state segment."""
+        return typing.cast(
+            mcu_pb.ParametersRequest, self.all_states[mcu_pb.ParametersRequest]
+        )
+
+    @property
+    def _parameters(self) -> mcu_pb.Parameters:
+        """Access the Parameters state segment."""
+        return typing.cast(
+            mcu_pb.Parameters, self.all_states[mcu_pb.Parameters]
+        )
+
+    # Timing
+
+    @property
+    def _time_step(self) -> float:
+        """Compute the time step."""
+        return self.current_time - self.previous_time
+
+    # Update methods
+
+    def update_parameters(self) -> None:
+        """Update the parameters."""
+
+    def update_clock(self, current_time: float) -> None:
+        """Update the internal state for timing."""
+        self.previous_time = self.current_time
+        self.current_time = current_time * 1000 - self.initial_time
+
+    def update_sensors(self) -> None:
+        """Update the simulated sensors."""
+
+    def update_actuators(self) -> None:
+        """Update the simulated actuators."""
+
+    # FiO2 simulation
+
+    def _update_fio2(self) -> None:
+        """Update FiO2 measurements."""
+        self._sensor_measurements.fio2 += (
+            (
+                self._parameters.fio2 - self._sensor_measurements.fio2
+            )
+            * self.fio2_responsiveness / self.SENSOR_UPDATE_INTERVAL
+        )
+        self._sensor_measurements.fio2 += self.fio2_noise * random.random()
+
+
+@attr.s
+class PCACSimulator(BreathingCircuitSimulator):
+    """Breathing circuit simulator in PC-AC mode."""
+
+    cycle_start_time: float = attr.ib(default=0)  # ms
+    cycle_period: float = attr.ib(default=2000.0)  # ms
+    insp_period: float = attr.ib(default=1000.0)  # ms
+    insp_responsiveness: float = attr.ib(default=0.05)  # ms
+    exp_responsiveness: float = attr.ib(default=0.05)  # ms
+    insp_init_flow_rate: float = attr.ib(default=120)  # L / min
+    exp_init_flow_rate: float = attr.ib(default=-120)  # L / min
+    insp_flow_responsiveness: float = attr.ib(default=0.02)  # ms
+    exp_flow_responsiveness: float = attr.ib(default=0.02)  # ms
+
+    def update_parameters(self) -> None:
+        """Implement BreathingCircuitSimulator.update_parameters."""
+        if self._parameters_request.rr > 0:
+            self._parameters.rr = self._parameters_request.rr
+        if self._parameters_request.ie > 0:
+            self._parameters.ie = self._parameters_request.ie
+        if self._parameters_request.pip > 0:
+            self._parameters.pip = self._parameters_request.pip
+        self._parameters.peep = self._parameters_request.peep
+        self._parameters.fio2 = self._parameters_request.fio2
+
+    def update_sensors(self) -> None:
+        """Implement BreathingCircuitSimulator.update_sensors."""
+        if self._time_step == 0:
+            return
+
+        self._sensor_measurements.time = int(self.current_time)
+        cycle_period = 60000 / self._parameters.rr
+        if self.current_time - self.cycle_start_time > cycle_period:
+            self._init_cycle(cycle_period)
+            self._update_cycle_measurements()
+
+        if self.current_time - self.cycle_start_time < self.insp_period:
+            self._update_airway_inspiratory()
+        else:
+            self._update_airway_expiratory()
+        self._update_fio2()
+
+    def _init_cycle(self, cycle_period: float) -> None:
+        """Initialize at the start of the cycle."""
+        self.cycle_start_time = self.current_time
+        self._sensor_measurements.flow = self.insp_init_flow_rate
+        self._sensor_measurements.volume = 0
+        self.insp_period = cycle_period / (1 + 1 / self._parameters.ie)
+
+    def _update_cycle_measurements(self) -> None:
+        """Update cycle measurements."""
+        self._cycle_measurements.rr = \
+            self._parameters.rr + random.random() - 0.5
+        self._cycle_measurements.peep = \
+            self._parameters.peep + random.random() - 0.5
+        self._cycle_measurements.pip = \
+            self._parameters.pip + random.random() - 0.5
+
+    def _update_airway_inspiratory(self) -> None:
+        """Update sensor measurements in inspiratory phase."""
+        self._sensor_measurements.paw += (
+            self._parameters.pip - self._sensor_measurements.paw
+        ) * self.insp_responsiveness / self._time_step
+        self._sensor_measurements.flow *= (
+            1 - self.insp_flow_responsiveness / self._time_step
+        )
+        self._sensor_measurements.volume += (
+            self._sensor_measurements.flow / 60 * self._time_step
+        )
+
+    def _update_airway_expiratory(self) -> None:
+        """Update sensor measurements in expiratory phase."""
+        self._sensor_measurements.paw += (
+            self._parameters.peep - self._sensor_measurements.paw
+        ) * self.exp_responsiveness / self._time_step
+        if self._sensor_measurements.flow >= 0:
+            self._sensor_measurements.flow = self.exp_init_flow_rate
+        else:
+            self._sensor_measurements.flow *= (
+                1 - self.exp_flow_responsiveness / self._time_step
+            )
+        self._sensor_measurements.volume += (
+            self._sensor_measurements.flow / 60 * self._time_step
+        )
+
+
 async def simulate_states(
         all_states: Mapping[
             Type[application.PBMessage], Optional[application.PBMessage]
         ]
 ) -> None:
     """Simulate evolution of all states."""
-    current_time = time.time() * 1000
-    initial_time = current_time
-    previous_time = current_time
-    time_step = current_time - previous_time
-    time_step = time_step if time_step > 0 else 1.0
-    cycle_start_time = 0.0
-    sensor_update_interval = 2.0  # ms
-    cycle_period = 2000.0  # ms
-    insp_period = 1000.0  # ms
-    insp_responsiveness = 0.05  # ms
-    exp_responsiveness = 0.05  # ms
-    insp_init_flow_rate = 120  # L / min
-    exp_init_flow_rate = -120  # L / min
-    insp_flow_responsiveness = 0.02  # ms
-    exp_flow_responsiveness = 0.02  # ms
-    fio2_responsiveness = 0.01  # ms
+    pc_ac = PCACSimulator(all_states=all_states)
     while True:
-        sensor_measurements = typing.cast(
-            mcu_pb.SensorMeasurements, all_states[mcu_pb.SensorMeasurements]
-        )
-        cycle_measurements = typing.cast(
-            mcu_pb.CycleMeasurements, all_states[mcu_pb.CycleMeasurements]
-        )
-        parameters = typing.cast(
-            mcu_pb.Parameters, all_states[mcu_pb.Parameters]
-        )
-        parameters_request = typing.cast(
-            mcu_pb.ParametersRequest, all_states[mcu_pb.ParametersRequest]
-        )
         # Parameters
-        if parameters_request.rr > 0:
-            parameters.rr = parameters_request.rr
-        if parameters_request.ie > 0:
-            parameters.ie = parameters_request.ie
-        if parameters_request.pip > 0:
-            parameters.pip = parameters_request.pip
-        parameters.peep = parameters_request.peep
-        parameters.fio2 = parameters_request.fio2
+        pc_ac.update_parameters()
         # Timing
-        await trio.sleep(sensor_update_interval / 1000)
-        previous_time = current_time
-        current_time = time.time() * 1000 - initial_time
-        time_step = current_time - previous_time
-        time_step = time_step if time_step > 0 else 1.0
-        sensor_measurements.time = int(current_time)
-        cycle_period = 60000 / parameters.rr
-        if current_time - cycle_start_time > cycle_period:
-            cycle_start_time = current_time
-            sensor_measurements.flow = insp_init_flow_rate
-            sensor_measurements.volume = 0
-            insp_period = cycle_period / (1 + 1 / parameters.ie)
-            # Cycle Measurements
-            cycle_measurements.rr = parameters.rr + random.random() - 0.5
-            cycle_measurements.peep = parameters.peep + random.random() - 0.5
-            cycle_measurements.pip = parameters.pip + random.random() - 0.5
-        # Airway
-        if current_time - cycle_start_time < insp_period:
-            sensor_measurements.paw += (
-                parameters.pip - sensor_measurements.paw
-            ) * insp_responsiveness / time_step
-            sensor_measurements.flow *= (
-                1 - insp_flow_responsiveness / time_step
-            )
-            sensor_measurements.volume += (
-                sensor_measurements.flow / 60 * time_step
-            )
-        else:
-            sensor_measurements.paw += (
-                parameters.peep - sensor_measurements.paw
-            ) * exp_responsiveness / time_step
-            if sensor_measurements.flow >= 0:
-                sensor_measurements.flow = exp_init_flow_rate
-            else:
-                sensor_measurements.flow *= (
-                    1 - exp_flow_responsiveness / time_step
-                )
-            sensor_measurements.volume += (
-                sensor_measurements.flow / 60 * time_step
-            )
-        # FiO2
-        sensor_measurements.fio2 += (
-            (
-                parameters.fio2 - sensor_measurements.fio2
-            )
-            * fio2_responsiveness / sensor_update_interval
-        )
-        sensor_measurements.fio2 += 0.005 * random.random()
+        await trio.sleep(pc_ac.SENSOR_UPDATE_INTERVAL / 1000)
+        pc_ac.update_clock(time.time())
+        pc_ac.update_sensors()
+        pc_ac.update_actuators()
 
 
 async def main() -> None:
