c58c85a9e231797aa97ac8b3c3b548834327314a
Praneet9 <praneetbomma@gmail.com>
2020-09-24 18:30:02 +0530

    added error handling

--- deploy/scripts/backend_service.sh
+++ deploy/scripts/backend_service.sh
@@ -4,8 +4,15 @@ echo "********** Setting up backend service **********"
 
 # Copying service file to systemd
 cd ~/pufferfish-vent-software/deploy
-sudo cp configs/pufferfish_backend.service /lib/systemd/system/
-sudo chmod 644 /lib/systemd/system/pufferfish_backend.service
+
+if [ 1 -eq $( ls configs/ | grep -c "pufferfish_backend.service" ) ]
+then
+    sudo cp configs/pufferfish_backend.service /etc/systemd/system/
+    sudo chmod 644 /etc/systemd/system/pufferfish_backend.service
+else
+    echo "The pufferfish_backend.service file doesn't exist"
+    exit 1
+fi
 
 # Enabling service
 sudo systemctl daemon-reload
