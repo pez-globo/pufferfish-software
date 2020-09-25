bc40ab36f913ffeeca9a3df46e0fcfe1702e6207
Ethan Li <lietk12@gmail.com>
2020-09-25 04:11:24 -0700

    Improve Endian based on Likhith's comments

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/STM32/Endian.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/STM32/Endian.h
@@ -20,42 +20,54 @@ namespace HAL {
  * @param hostValue a 64-bit number in host-order
  * @return the given number in network-order
  */
-uint64_t hton(uint64_t host_value);
+inline uint64_t hton(uint64_t host_value) {
+  return __builtin_bswap64(host_value);
+}
 
 /**
  * Convert endianness of a 64-bit data in host-order to network-order
  * @param networkValue a 64-bit number in network-order
  * @return the given number in host-order
  */
-uint64_t ntoh(uint64_t network_value);
+inline uint64_t ntoh(uint64_t network_value) {
+  return __builtin_bswap64(network_value);
+}
 
 /**
  * Convert endianness of a 32-bit data in host-order to network-order
  * @param hostValue a 32-bit number in host-order
  * @return the given number in network-order
  */
-uint32_t hton(uint32_t host_value);
+inline uint32_t hton(uint32_t host_value) {
+  return __builtin_bswap32(host_value);
+}
 
 /**
  * Convert endianness of a 32-bit data in host-order to network-order
  * @param networkValue a 32-bit number in network-order
  * @return the given number in host-order
  */
-uint32_t ntoh(uint32_t network_value);
+inline uint32_t ntoh(uint32_t network_value) {
+  return __builtin_bswap32(network_value);
+}
 
 /**
  * Convert endianness of a 16-bit data in host-order to network-order
  * @param hostValue a 16-bit number in host-order
  * @return the given number in network-order
  */
-uint16_t hton(uint16_t host_value);
+inline uint16_t hton(uint16_t host_value) {
+  return __builtin_bswap16(host_value);
+}
 
 /**
  * Convert endianness of a 16-bit data in host-order to network-order
  * @param networkValue a 16-bit number in network-order
  * @return the given number in host-order
  */
-uint16_t ntoh(uint16_t network_value);
+inline uint16_t ntoh(uint16_t network_value) {
+  return __builtin_bswap16(network_value);
+}
 
 }  // namespace HAL
 }  // namespace Pufferfish
