2d597f8d3332a48b2223a2929db2724ae96b4351
Praneet9 <praneetbomma@gmail.com>
2020-10-01 10:55:00 +0530

    absolute path for backend file

--- deploy/scripts/backend_service.sh
+++ deploy/scripts/backend_service.sh
@@ -5,10 +5,13 @@ echo "********** Setting up backend service **********"
 # Getting absolute path of config files
 script_dir=$(dirname $(realpath $0))
 config_dir=$script_dir/../configs
+backend_dir=$script_dir/../../backend/ventserver
+backend_file=$(realpath $backend_dir)/simulation.py
 
 if [ 1 -eq $( ls $config_dir | grep -c "pufferfish_backend.service" ) ]
 then
     sudo cp $config_dir/pufferfish_backend.service /etc/systemd/system/
+    sudo sed -i "s|python_file_absolute_path|$backend_file|g" /etc/systemd/system/pufferfish_backend.service
     sudo chmod 644 /etc/systemd/system/pufferfish_backend.service
 else
     echo "The pufferfish_backend.service file doesn't exist"
