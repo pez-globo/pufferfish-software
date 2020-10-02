0af2f1264e46b00f6665c44e91fa382817424c37
Praneet9 <praneetbomma@gmail.com>
2020-10-02 19:37:01 +0530

    using LABEL in fstab

--- deploy/scripts/usb_logging.sh
+++ deploy/scripts/usb_logging.sh
@@ -12,7 +12,7 @@ echo -e "\n${SUCCESS}********** Setting up USB Logging Device **********\n$NC"
 script_dir=$(dirname $(realpath $0))
 config_dir=$script_dir/../configs
 
-media_boot="\n/dev/disk/by-label/LOGS      /media/pi/LOGS       auto    rw,nosuid,nodev,x-systemd.device-timeout=5,noauto,nofail          0       0"
+media_boot="\nLABEL=LOGS      /media/pi/LOGS       auto    rw,nosuid,nodev,x-systemd.device-timeout=5,noauto,nofail          0       0"
 fstab_file="/etc/fstab"
 
 # Adding LOGS mount point to /etc/fstab
