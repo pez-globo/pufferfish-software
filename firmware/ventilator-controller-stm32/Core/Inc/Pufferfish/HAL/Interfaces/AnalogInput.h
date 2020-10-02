014c862a3270192e476f35b70ced4895d4ada232
hemanthgs <hemanth.gowda@avinsystems.com>
2020-10-02 11:12:59 +0530

    Resolved file header format update for AnalogInput.h

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/Interfaces/AnalogInput.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/Interfaces/AnalogInput.h
@@ -18,22 +18,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "Pufferfish/HAL/Interfaces/PWM.h"
-
-namespace Pufferfish::HAL {
-
-PWMStatus PWM::set_duty_cycle(float duty) {
-  if (duty < 0.0 || 1.0 < duty) {
-    return PWMStatus::invalid_duty_cycle;
-  }
-
-  set_duty_cycle_raw(static_cast<uint32_t>(duty * get_max_duty_cycle()));
-  return PWMStatus::ok;
-}
-
-}  // namespace Pufferfish::HAL
-
-
 #pragma once
 
 #include <cstdint>
