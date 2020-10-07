f6bcc20529aebef205f28778800f545377b3313a
Praneet9 <praneetbomma@gmail.com>
2020-10-07 18:25:19 +0530

    removed symbolic link fix for RO mode

--- deploy/scripts/nginx_setup.sh
+++ deploy/scripts/nginx_setup.sh
@@ -11,9 +11,6 @@ echo -e "\n${SUCCESS}********** Setting up nginx **********\n${NC}"
 sudo apt update
 sudo apt install nginx -y
 
-# Symbolic Link to nginx for read-only filesystem
-sudo ln -s /tmp /var/log/nginx
-
 # Getting absolute path of frontend files
 script_dir=$(dirname $(realpath $0))
 frontend_dir=$script_dir/../../frontend
