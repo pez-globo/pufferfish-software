d1e1fa99f8f6c88b980fccb8f08fe915039fd769
Praneet9 <praneetbomma@gmail.com>
2020-10-08 19:22:28 +0530

    added error handling and bug fixes

--- deploy/scripts/overlayfs.sh
+++ deploy/scripts/overlayfs.sh
@@ -7,24 +7,42 @@ config_dir=$script_dir/../configs
 sudo apt update
 sudo apt install initramfs-tools -y
 
-sudo sed -i 's/console=tty1/console=tty3/g' /boot/cmdline.txt
+if [ 0 -eq $( cat /boot/cmdline.txt | grep -c "console=tty3" ) ]
+then
+  sudo sed -i 's/console=tty1/console=tty3/g' /boot/cmdline.txt
+fi
 
 if ! grep $config_dir/overlay /etc/initramfs-tools/modules > /dev/null; then
-  echo $config_dir/overlay | sudo tee -a /etc/initramfs-tools/modules
+  cat $config_dir/overlay | sudo tee -a /etc/initramfs-tools/modules
 fi
 
 sudo cp $config_dir/overlay /etc/initramfs-tools/scripts
 
-sudo update-initramfs -c -k $(uname -r)
-sudo mv /boot/initrd.img-$(uname -r) /boot/initrd7.img
+if [ 1 -eq $( ls /boot/ | grep -c "initrd7.img" ) ]
+then
+  sudo update-initramfs -c -k $(uname -r)
+  sudo mv /boot/initrd.img-$(uname -r) /boot/initrd7.img
+else
+  echo -e "Updated initramfs already exists"
+fi
 
 sudo sed -e "s/initramfs.*//" -i /boot/config.txt
-echo initramfs initrd7.img | sudo tee -a /boot/config.txt
 
-sudo cp /boot/cmdline.txt /boot/cmdline.txt.orig
-sudo sed -e "s/\(.*\)/boot=overlay \1/" -i /boot/cmdline.txt
-sudo cp /boot/cmdline.txt /boot/cmdline.txt.overlay
+if [ 0 -eq $( cat /boot/config.txt | grep -c "initramfs initrd7.img" ) ]
+then
+  echo initramfs initrd7.img | sudo tee -a /boot/config.txt
+fi
 
-sudo cp overctl /usr/local/sbin
+if [ 0 -eq $( ls /boot/ | grep -c "cmdline.txt.overlay" ) ]
+then
+  sudo cp /boot/cmdline.txt /boot/cmdline.txt.orig
+  sudo sed -e "s/\(.*\)/boot=overlay \1/" -i /boot/cmdline.txt
+  sudo cp /boot/cmdline.txt /boot/cmdline.txt.overlay
+fi
 
-sudo sed -e "s/\(.*\/boot.*\)defaults\(.*\)/\1defaults,ro\2/" -i /etc/fstab
+sudo cp $config_dir/overctl /usr/local/sbin
+
+if [ 0 -eq $( cat /etc/fstab | grep -c "defaults,ro" ) ]
+then
+  sudo sed -e "s/\(.*\/boot.*\)defaults\(.*\)/\1defaults,ro\2/" -i /etc/fstab
+fi
