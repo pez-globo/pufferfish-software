e2ae08132270ce90da0d516f1ac0706984b93858
Praneet9 <praneetbomma@gmail.com>
2020-10-27 21:31:17 +0530

    exiting script on failure

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,42 +2,43 @@
 
 # Getting absolute path of script
 script_path=$(dirname $(realpath $0))
+. $script_path/scripts/helper.sh
 
 # Install required components
-$script_path/install.sh "deploy"
+$script_path/install.sh "deploy" || exit_script "Development components installation failed"
 
 # Setup Nginx web server for serving frontend
-$script_path/scripts/nginx_setup.sh
+$script_path/scripts/nginx_setup.sh || exit_script "Nginx webserver setup failed"
 
 # Setup OS Boot Screen
-$script_path/scripts/boot_screen.sh
+$script_path/scripts/boot_screen.sh || exit_script "Boot Screen setup failed"
 
 # Setup backend autostarting service on boot
-$script_path/scripts/backend_service.sh
+$script_path/scripts/backend_service.sh || exit_script "Backend service setup failed"
 
 # Setup configuration to disable screen blanking and hiding cursor on idle
-$script_path/scripts/configure_kiosk.sh
+$script_path/scripts/configure_kiosk.sh || exit_script "UI configuration setup failed"
 
 # Setup systemd service to start Chromium Browser in Kiosk Mode on boot
-$script_path/scripts/kiosk.sh
+$script_path/scripts/kiosk.sh || exit_script "Kiosk service setup failed"
 
 # Setup Logging in USB
-$script_path/scripts/usb_logging.sh
+$script_path/scripts/usb_logging.sh || exit_script "USB Logging setup failed"
 
 # Modify eeprom to boot from USB Mass Storage by default
-$script_path/scripts/live_usb.sh
+$script_path/scripts/live_usb.sh || exit_script "USB Mass Storage setup failed"
 
 # Modify eeprom to setup watchdog to handle Raspberry Pi failures and crashes
-$script_path/scripts/setup_watchdog.sh
+$script_path/scripts/setup_watchdog.sh || exit_script "Watchdog setup failed"
 
 # Enable Overlayfs and change /boot partition to read-only
-$script_path/scripts/overlayfs.sh
+$script_path/scripts/overlayfs.sh || exit_script "Read-only setup failed"
 
 # Disable unnecessary background services
-$script_path/scripts/disable_services.sh
+$script_path/scripts/disable_services.sh || exit_script "Disabling background services failed"
 
 # Setup security protocols
-$script_path/scripts/security.sh
+$script_path/scripts/security.sh || exit_script "Security protocols setup failed"
 
 echo -n "Reboot required for changes to take effect. Do you want to reboot now? [y/N]: "
 read answer
--- deploy/install.sh
+++ deploy/install.sh
@@ -2,24 +2,25 @@
 
 # Getting absolute path of script
 script_path=$(dirname $(realpath $0))
+. $script_path/scripts/helper.sh
 
 # Setup pyenv
-$script_path/scripts/pyenv.sh
+$script_path/scripts/pyenv.sh || exit_script "Pyenv installation failed"
 
 # Setup poetry and install required libraries
-$script_path/scripts/poetry.sh
+$script_path/scripts/poetry.sh || exit_script "Poetry installation failed"
 
 # Setup pigpio dependency for backend
-$script_path/scripts/pigpio.sh
+$script_path/scripts/pigpio.sh || exit_script "Pigpio installation failed"
 
 # Setup Node
-$script_path/scripts/node.sh
+$script_path/scripts/node.sh || exit_script "Node installation failed"
 
 # Install Roboto Fonts
-$script_path/scripts/install_fonts.sh
+$script_path/scripts/install_fonts.sh || exit_script "Roboto Fonts installation failed"
 
 # Setup yarn and install required libraries
-$script_path/scripts/yarn.sh
+$script_path/scripts/yarn.sh || exit_script "Yarn installation failed"
 
 if [[ "$@" != "deploy" ]]
 then
