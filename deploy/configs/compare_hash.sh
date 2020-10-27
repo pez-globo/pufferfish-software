a14d0f63128f171883098841bc66b5d4ac77ac9b
Praneet9 <praneetbomma@gmail.com>
2020-10-27 15:36:44 +0530

    shutdown on tampering detection

--- deploy/configs/compare_hash.sh
+++ deploy/configs/compare_hash.sh
@@ -6,17 +6,17 @@ error_msg="Data Tampering check failed! \nDisabled Backend and Kiosk service."
 if [ 1 -eq $( ls /opt/pufferfish | grep -c "hash_value" ) ]
 then
     original_value=$(cat /opt/pufferfish/hash_value)
-    calculated_value=$(/usr/bin/python3 /opt/pufferfish/hash_check.py)
+    calculated_value=$(su -l pi -c '/usr/bin/python3 /opt/pufferfish/hash_check.py')
 
     if [[ "$original_value" == "$calculated_value" ]]
     then
         exit 0
     else
         zenity --error --text "$error_msg" --width=300 --height=150
-        exit 1
+        poweroff
     fi
 
 else
     zenity --error --text "$error_msg" --width=300 --height=150
-    exit 1
+    poweroff
 fi
