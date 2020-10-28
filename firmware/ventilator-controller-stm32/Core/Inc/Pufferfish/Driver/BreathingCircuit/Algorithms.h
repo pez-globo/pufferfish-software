27f02294f30e8c358bc787aac6ce4ca9f6222403
Ethan Li <lietk12@gmail.com>
2020-10-28 13:46:53 -0700

    Fix i_max to be a more reasonable value vs. i_gain

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/BreathingCircuit/Algorithms.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/BreathingCircuit/Algorithms.h
@@ -22,8 +22,8 @@ class PI {
   static constexpr float out_min = 0;
   static constexpr float p_gain = 0.00001;
   static constexpr float i_gain = 0.0002;
-  static constexpr float i_max = 200 * i_gain;
-  static constexpr float i_min = 0;
+  static constexpr float i_max = out_max / i_gain;
+  static constexpr float i_min = out_min / i_gain;
 
   float error_ = 0;
   float error_integral_ = 0;
