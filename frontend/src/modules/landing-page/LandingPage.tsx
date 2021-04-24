b283f95895ce8cd04dab97d560af52e7c1d4b5c5
Ethan Li <lietk12@gmail.com>
2021-04-23 18:52:20 -0700

    Add emergency use notice to landing page

--- frontend/src/modules/landing-page/LandingPage.tsx
+++ frontend/src/modules/landing-page/LandingPage.tsx
@@ -7,7 +7,7 @@ const useStyles = makeStyles(() => ({
   slideHead: {
     margin: '0px',
     textAlign: 'left',
-    marginBottom: '10px',
+    marginBottom: '2.5rem',
 
     '& ul': {
       padding: '0px 20px',
@@ -22,8 +22,13 @@ const useStyles = makeStyles(() => ({
     backgroundColor: '#042139',
     padding: '20px',
     borderRadius: '10px',
-    minHeight: '235px',
+    minHeight: '275px',
   },
+  slideText: {
+    textAlign: 'left',
+    fontWeight: 'normal',
+    fontSize: '1.5rem',
+  }
 }));
 
 export const LandingPage = (): JSX.Element => {
@@ -36,39 +41,73 @@ export const LandingPage = (): JSX.Element => {
         position: 'relative',
       }}
     >
-      <Carousel
-        slides={[
-          <div>
-            <h1 className={classes.slideHead}>Invasive Modes</h1>
-            <Grid item xs direction="row" container className={classes.slideContent}>
-              <Grid item xs className={classes.slideHead}>
-                <h4>Pressure Controlled</h4>
-                <ul>
-                  <li>PC-AC</li>
-                  <li>PSV</li>
-                  <li>PC-SIMV</li>
-                  <li>Bi-level</li>
-                </ul>
-              </Grid>
-              <Grid item xs className={classes.slideHead}>
-                <h4>Volume Controlled</h4>
-                <ul>
-                  <li>VC-AC</li>
-                  <li>VC-SIMV</li>
-                </ul>
-              </Grid>
-              <Grid item xs className={classes.slideHead}>
-                <h4>Miscellaneous</h4>
-                <ul>
-                  <li>PRVC</li>
-                </ul>
-              </Grid>
+      <Carousel slides={[
+        /*<div>
+          <h1 className={classes.slideHead}>Invasive Modes</h1>
+          <Grid item xs direction="row" container className={classes.slideContent}>
+            <Grid item xs className={classes.slideHead}>
+              <h4>Pressure Controlled</h4>
+              <ul>
+                <li>PC-AC</li>
+                <li>PSV</li>
+                <li>PC-SIMV</li>
+                <li>Bi-level</li>
+              </ul>
             </Grid>
-          </div>,
-          <div />,
-          <div />,
-        ]}
-      />
+            <Grid item xs className={classes.slideHead}>
+              <h4>Volume Controlled</h4>
+              <ul>
+                <li>VC-AC</li>
+                <li>VC-SIMV</li>
+              </ul>
+            </Grid>
+            <Grid item xs className={classes.slideHead}>
+              <h4>Miscellaneous</h4>
+              <ul>
+                <li>PRVC</li>
+              </ul>
+            </Grid>
+          </Grid>
+        </div>,*/
+        <div>
+          <h1 className={classes.slideHead}>Emergency Use Notice</h1>
+          <Grid item xs direction="row" container className={classes.slideContent}>
+            <Grid item xs className={classes.slideText}>
+              <p>
+                This Pufferfish ventilator is running a test version of software
+                with known problems, limitations, and missing safety measures. For this
+                reason, any person connected to this ventilator should always be
+                supervised by someone trained in what to do if a problem occurs.
+              </p>
+              <p>
+                This ventilator provides oxygen therapy and may be considered for
+                use if no safer alternatives are available. It should not be used
+                outside of extreme crisis situations.
+              </p>
+            </Grid>
+          </Grid>
+        </div>,
+        <div>
+          <h1 className={classes.slideHead}>Capabilities</h1>
+          <Grid item xs direction="row" container className={classes.slideContent}>
+            <Grid item xs className={classes.slideText}>
+              <p>
+                This version of software provides a visual alarm when the oxygen
+                supply runs out, and it allows adjusting FiO2 and flow rate. Because
+                alarms do not generate sounds, the ventilator must always be watched
+                in case of alarms.
+              </p>
+              <p>
+                This ventilator has a small internal battery in case of power outage.
+                Because the current version of software does not have any way to
+                read the charge level of the battery, it will shut off without
+                warning if it runs out of power. Thus, the ventilator should never be
+                disconnected from its power source.
+              </p>
+            </Grid>
+          </Grid>
+        </div>,
+      ]} />
     </div>
   );
 };
