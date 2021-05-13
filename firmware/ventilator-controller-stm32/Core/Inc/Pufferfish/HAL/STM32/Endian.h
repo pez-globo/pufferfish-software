c08b65d0f240452e7e8db6bf476c8e37573cfad0
Raavi <raavi.lagoo@gmail.com>
2021-05-13 12:46:35 +0530

    Endian.h implementation from Interfaces folder

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/STM32/Endian.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/HAL/STM32/Endian.h
@@ -18,10 +18,57 @@
 
 namespace Pufferfish::HAL::STM32 {
 
-constexpr uint64_t hton(uint64_t host_value); 
-constexpr uint64_t ntoh(uint64_t network_value); 
-constexpr uint32_t hton(uint32_t host_value);
-constexpr uint32_t ntoh(uint32_t network_value); 
-constexpr uint16_t hton(uint16_t host_value);
-constexpr uint16_t ntoh(uint16_t network_value);
+/**
+ * Convert endianness of a 64-bit data in host-order to network-order
+ * @param hostValue a 64-bit number in host-order
+ * @return the given number in network-order
+ */
+constexpr uint64_t hton(uint64_t host_value) noexcept {
+  return __builtin_bswap64(host_value);
+}
+
+/**
+ * Convert endianness of a 64-bit data in host-order to network-order
+ * @param networkValue a 64-bit number in network-order
+ * @return the given number in host-order
+ */
+constexpr uint64_t ntoh(uint64_t network_value) noexcept {
+  return __builtin_bswap64(network_value);
+}
+
+/**
+ * Convert endianness of a 32-bit data in host-order to network-order
+ * @param hostValue a 32-bit number in host-order
+ * @return the given number in network-order
+ */
+constexpr uint32_t hton(uint32_t host_value) noexcept {
+  return __builtin_bswap32(host_value);
+}
+
+/**
+ * Convert endianness of a 32-bit data in host-order to network-order
+ * @param networkValue a 32-bit number in network-order
+ * @return the given number in host-order
+ */
+constexpr uint32_t ntoh(uint32_t network_value) noexcept {
+  return __builtin_bswap32(network_value);
+}
+
+/**
+ * Convert endianness of a 16-bit data in host-order to network-order
+ * @param hostValue a 16-bit number in host-order
+ * @return the given number in network-order
+ */
+constexpr uint16_t hton(uint16_t host_value) noexcept {
+  return __builtin_bswap16(host_value);
+}
+
+/**
+ * Convert endianness of a 16-bit data in host-order to network-order
+ * @param networkValue a 16-bit number in network-order
+ * @return the given number in host-order
+ */
+constexpr uint16_t ntoh(uint16_t network_value) noexcept {
+  return __builtin_bswap16(network_value);
+}
 }  // namespace Pufferfish::HAL::STM32
