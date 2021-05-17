pez-globo/pufferfish-software

commit 0b9946d67a3eb921c717521998707060777729fb

frontend: Improve set alarms screen  (#338)
* add content for apply changes modal popup

* disable button on change in alarm limits

* frontend: add discard button

some improvements

* fix eslint errors

* update button styling and improve code

* fix set alarm limit values

* fix bugs and update implementation after store refactor

* toolbar: add confirmation dialog on dashboard back button

* rename setAlarmLimits to setAlarmLimitsRequestStandby

* add selector for checking unsaved changes in alarm limits

* initialize useState with unsavedChanges alarm limits selector

* remove null checks for selector

* multistepwizard: show unsaved alarm limit changes in HFNC Control

* frontend: fix frontend console errors/warnings

* Failed prop type: The prop `alignItems` of `Grid` must be used on `container`.
* Following component has two or more children with the same key attribute: "indicator-[object Object]". This may cause glitches and misbehavior in rendering process. Component:

* frontend: MessageType Request to standby

* alarms: only update standby values in dashboard

* this change fixes a bug where opening event log and changing alarm limits from there and then discarding
would discard unsaved changes on set alarms page screeen as well

* apply lint autofix

* reducer: pass rotary encoder old state properly

* frontend: Fix a bug where changing alarm limits from active alarms modal did not reflect in alarms page

* i was using a useState and not updating it later, i removed that now

* Revert "reducer: pass rotary encoder old state properly"

This reverts commit 3361f5e77cb2b0b5ccfdd394a75f029d9e95e530.

this was just a test

* alarms: rename a few functions

* frontend: rename commitStandbyRequest to commitDraftRequest

* frontend: update selectors and some renaming

* toolbar: resolve review comments

* frontend: rename a few more things

* frontend: udpate selectors and use alarmLimitsRequest

frontend/src/modules/displays/MultiStepWizard.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { makeStyles, Theme, Grid, Tabs, Tab, Button, Typography } from '@material-ui/core';
 import ReplyIcon from '@material-ui/icons/Reply';
 // import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { useLocation } from 'react-router-dom';
 import ModalPopup from '../controllers/ModalPopup';
 import { getcurrentStateKey, getMultiPopupOpenState, setMultiPopupOpen } from '../app/Service';
 import {
@@ -20,8 +21,9 @@ import { BPM, LMIN, PERCENT } from '../info/units';
 import { AlarmModal } from '../controllers';
 import { ParametersRequest, AlarmLimitsRequest } from '../../store/controller/proto/mcu_pb';
 import { MessageType } from '../../store/controller/types';
-import { commitRequest, commitStandbyRequest } from '../../store/controller/actions';
+import { commitRequest, commitDraftRequest } from '../../store/controller/actions';
 import store from '../../store';
+import { DASHBOARD_ROUTE } from '../navigation/constants';
 
 interface Data {
   stateKey: string;
@@ -280,6 +282,7 @@ const determineInput = (stateKey: string): Data | undefined => {
 const MultiStepWizard = (): JSX.Element => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const location = useLocation();
   const [open, setOpen] = React.useState(false);
   const [confirmOpen, setConfirmOpen] = React.useState(false);
   const [cancelOpen, setCancelOpen] = React.useState(false);
@@ -363,6 +366,18 @@ const MultiStepWizard = (): JSX.Element => {
       const param = multiParams.find((param: Data) => param.stateKey === parameter.stateKey);
       if (param) param.alarmValues = [min, max];
       parameter.alarmValues = [min, max];
+      // we want to dispatch commitDraftRequest to AlarmLimitsRequest to show the unsaved alarm limit changes
+      // in the HFNC control (value info right corner), but doing so when in set alarms page will discard the unsaved changes
+      // which is unwanted, thus only do this when we are on dashboard.
+      if (location.pathname === DASHBOARD_ROUTE.path) {
+        const update = {
+          [parameter.stateKey]: {
+            lower: min,
+            upper: max,
+          },
+        };
+        dispatch(commitDraftRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, update));
+      }
       if (isAnyChanges()) {
         setIsSubmitDisabled(false);
       } else {
@@ -427,7 +442,7 @@ const MultiStepWizard = (): JSX.Element => {
       if (parameter.isSetvalEnabled) {
         const update = { [parameter.stateKey]: parameter.setValue };
         dispatch(commitRequest<ParametersRequest>(MessageType.ParametersRequest, update));
-        dispatch(commitStandbyRequest<ParametersRequest>(MessageType.ParametersRequest, update));
+        dispatch(commitDraftRequest<ParametersRequest>(MessageType.ParametersRequest, update));
       }
       if (parameter.isAlarmEnabled && parameter.alarmValues.length) {
         const update = {
@@ -437,7 +452,7 @@ const MultiStepWizard = (): JSX.Element => {
           },
         };
         dispatch(commitRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, update));
-        dispatch(commitStandbyRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, update));
+        dispatch(commitDraftRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, update));
       }
     });
     setMultiParams([]);
@@ -604,7 +619,7 @@ const MultiStepWizard = (): JSX.Element => {
                 <Typography variant="h4">Confirm New Changes?</Typography>
               </Grid>
             </Grid>
-            <Grid item alignItems="center" className={classes.marginContent}>
+            <Grid item className={classes.marginContent}>
               {multiParams.map((param: Data) => {
                 if (param.isSetvalEnabled) {
                   if (param.setValue !== param.setValueActual) {
@@ -642,7 +657,7 @@ const MultiStepWizard = (): JSX.Element => {
                 <Typography variant="h4">Keep Previous Values?</Typography>
               </Grid>
             </Grid>
-            <Grid item alignItems="center" className={classes.marginContent}>
+            <Grid item className={classes.marginContent}>
               {multiParams.map((param: Data) => {
                 if (param.isSetvalEnabled) {
                   if (param.setValue !== param.setValueActual) {

frontend/src/modules/alarms/AlarmsPage.tsx
@@ -3,10 +3,13 @@ import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 import React, { RefObject, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { commitRequest, commitStandbyRequest } from '../../store/controller/actions';
+import { commitRequest, commitDraftRequest } from '../../store/controller/actions';
 import { AlarmLimitsRequest, VentilationMode, Range } from '../../store/controller/proto/mcu_pb';
 import {
-  getAlarmLimitsRequestStandby,
+  getAlarmLimitsRequest,
+  getAlarmLimitsRequestDraft,
+  getAlarmLimitsRequestUnsaved,
+  getAlarmLimitsUnsavedKeys,
   getParametersIsVentilating,
   getParametersRequestMode,
 } from '../../store/controller/selectors';
@@ -128,7 +131,7 @@ const Alarm = ({
   // discard changes (which means that any alarm limits being persisted in
   // AlarmLimitsRequestStandby would need to be reset using the values from
   // AlarmLimitsRequest, when the user wants to discard values). This could
-  // be done with by dispatching a commitStandbyRequest action with the
+  // be done with by dispatching a commitDraftRequest action with the
   // alarmLimitsRequest selector as the update field.
   const range =
     alarmLimits === null
@@ -187,7 +190,7 @@ const Alarm = ({
               height: '100%',
             }}
           >
-            <Grid alignItems="center" item className={classes.alarmValue}>
+            <Grid item className={classes.alarmValue}>
               <Typography align="center" variant="h4">
                 {rangeValues[0] === undefined || Number.isNaN(rangeValues[0])
                   ? '--'
@@ -219,7 +222,7 @@ const Alarm = ({
               height: '100%',
             }}
           >
-            <Grid alignItems="center" item className={classes.alarmValue}>
+            <Grid item className={classes.alarmValue}>
               <Typography align="center" variant="h4">
                 {rangeValues[1] === undefined || Number.isNaN(rangeValues[1])
                   ? '--'
@@ -292,35 +295,58 @@ export const AlarmsPage = (): JSX.Element => {
     setPage(value);
   };
 
-  const alarmLimitsRequestStandby = useSelector(getAlarmLimitsRequestStandby);
   const dispatch = useDispatch();
   const currentMode = useSelector(getParametersRequestMode);
   const ventilating = useSelector(getParametersIsVentilating);
-  const setAlarmLimits = (data: Partial<AlarmLimitsRequest>) => {
-    dispatch(commitStandbyRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, data));
+  const alarmLimitsRequestDraftSelect = useSelector(getAlarmLimitsRequestDraft);
+  const alarmLimitsRequestSelect = useSelector(getAlarmLimitsRequest);
+  const alarmLimitsRequestUnsaved = useSelector(getAlarmLimitsRequestUnsaved);
+  const alarmLimitsUnsavedKeys = useSelector(getAlarmLimitsUnsavedKeys);
+  const alarmLimitsRequest = (alarmLimitsRequestSelect as unknown) as Record<string, Range>;
+  const alarmLimitsRequestDraft = (alarmLimitsRequestDraftSelect as unknown) as Record<
+    string,
+    Range
+  >;
+  const setAlarmLimitsRequestDraft = (data: Partial<AlarmLimitsRequest>) => {
+    dispatch(commitDraftRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, data));
   };
   const applyChanges = () => {
-    if (alarmLimitsRequestStandby === null) {
+    if (alarmLimitsRequestDraft === null) {
       return;
     }
 
     dispatch(
-      commitRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, alarmLimitsRequestStandby),
+      commitRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, alarmLimitsRequestDraft),
     );
   };
   const alarmConfig = alarmConfiguration(currentMode);
-  const [open, setOpen] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
+  const [discardOpen, setDiscardOpen] = useState(false);
 
   const handleClose = () => {
-    setOpen(false);
+    setConfirmOpen(false);
+  };
+
+  const handleDiscardClose = () => {
+    setDiscardOpen(false);
   };
 
   const handleConfirm = () => {
-    setOpen(false);
+    setConfirmOpen(false);
     applyChanges();
   };
-  const handleOpen = () => {
-    setOpen(true);
+
+  const handleDiscardConfirm = () => {
+    setDiscardOpen(false);
+    if (alarmLimitsRequest !== null) setAlarmLimitsRequestDraft(alarmLimitsRequest);
+  };
+
+  const handleConfirmOpen = () => {
+    setConfirmOpen(true);
+  };
+
+  const handleDiscardOpen = () => {
+    setDiscardOpen(true);
   };
 
   useEffect(() => {
@@ -346,8 +372,8 @@ export const AlarmsPage = (): JSX.Element => {
                   max={alarm.max || 100}
                   stateKey={alarm.stateKey}
                   step={alarm.step || 1}
-                  alarmLimits={alarmLimitsRequestStandby}
-                  setAlarmLimits={setAlarmLimits}
+                  alarmLimits={alarmLimitsRequestDraftSelect}
+                  setAlarmLimits={setAlarmLimitsRequestDraft}
                 />
               );
             })}
@@ -374,18 +400,30 @@ export const AlarmsPage = (): JSX.Element => {
             <Grid item style={{ textAlign: 'right' }}>
               {ventilating ? (
                 <Button
-                  onClick={handleOpen}
+                  onClick={handleDiscardOpen}
+                  color="primary"
+                  variant="contained"
+                  className={classes.applyButton}
+                  disabled={!alarmLimitsRequestUnsaved}
+                >
+                  Cancel
+                </Button>
+              ) : null}
+              {ventilating ? (
+                <Button
+                  onClick={handleConfirmOpen}
                   color="secondary"
                   variant="contained"
                   className={classes.applyButton}
+                  disabled={!alarmLimitsRequestUnsaved}
                 >
-                  Apply Changes
+                  Submit
                 </Button>
               ) : null}
               <ModalPopup
                 withAction={true}
                 label="Set Alarms"
-                open={open}
+                open={confirmOpen}
                 onClose={handleClose}
                 onConfirm={handleConfirm}
               >
@@ -396,7 +434,55 @@ export const AlarmsPage = (): JSX.Element => {
                         <Typography variant="h4">Confirm New Changes?</Typography>
                       </Grid>
                     </Grid>
-                    <Grid item alignItems="center" className={classes.marginContent} />
+                    <Grid item className={classes.marginContent}>
+                      {alarmConfig.map((param) => {
+                        if (alarmLimitsRequestDraft !== null) {
+                          if (alarmLimitsUnsavedKeys.includes(param.stateKey)) {
+                            return (
+                              <Typography variant="subtitle1">{`Change ${
+                                param.label
+                              } alarm range to ${alarmLimitsRequestDraft[param.stateKey].lower} -
+                                  ${alarmLimitsRequestDraft[param.stateKey].upper}?`}</Typography>
+                            );
+                          }
+                        }
+                        return <React.Fragment />;
+                      })}
+                    </Grid>
+                    <Grid item className={classes.marginContent} />
+                  </Grid>
+                </Grid>
+              </ModalPopup>
+              <ModalPopup
+                withAction={true}
+                label="Set Alarms"
+                open={discardOpen}
+                onClose={handleDiscardClose}
+                onConfirm={handleDiscardConfirm}
+              >
+                <Grid container alignItems="center">
+                  <Grid container alignItems="center" justify="center">
+                    <Grid container alignItems="center" className={classes.marginHeader}>
+                      <Grid item xs>
+                        <Typography variant="h4">Keep Previous Values?</Typography>
+                      </Grid>
+                    </Grid>
+                    <Grid item className={classes.marginContent}>
+                      {alarmConfig.map((param) => {
+                        if (alarmLimitsRequest !== null) {
+                          if (alarmLimitsUnsavedKeys.includes(param.stateKey)) {
+                            return (
+                              <Typography variant="subtitle1">{`Keep ${
+                                param.label
+                              } alarm range to ${alarmLimitsRequest[param.stateKey].lower} -
+                                ${alarmLimitsRequest[param.stateKey].upper}?`}</Typography>
+                            );
+                          }
+                        }
+                        return <React.Fragment />;
+                      })}
+                    </Grid>
+                    <Grid item className={classes.marginContent} />
                   </Grid>
                 </Grid>
               </ModalPopup>

frontend/src/modules/app/ToolBar.tsx
@@ -1,25 +1,30 @@
-import { AppBar, Button, Grid } from '@material-ui/core';
+import { AppBar, Button, Grid, Typography } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import React, { useCallback, useEffect, useState } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { getClockTime } from '../../store/app/selectors';
-import { commitRequest } from '../../store/controller/actions';
+import { commitRequest, commitDraftRequest } from '../../store/controller/actions';
 import {
   ParametersRequest,
   VentilationMode,
   AlarmLimitsRequest,
+  Range,
 } from '../../store/controller/proto/mcu_pb';
 import {
   getBatteryPowerLeft,
   getChargingStatus,
   getParametersIsVentilating,
   getParametersRequestMode,
-  getParametersRequestStandby,
-  getAlarmLimitsRequestStandby,
+  getParametersRequestDraft,
+  getAlarmLimitsRequestDraft,
   getBackendInitialized,
+  getAlarmLimitsRequestUnsaved,
+  getAlarmLimitsUnsavedKeys,
+  getAlarmLimitsRequest,
 } from '../../store/controller/selectors';
 import { MessageType } from '../../store/controller/types';
+import { ModalPopup } from '../controllers/ModalPopup';
 import ViewDropdown from '../dashboard/views/ViewDropdown';
 import { BackIcon } from '../icons';
 import ClockIcon from '../icons/ClockIcon';
@@ -42,6 +47,16 @@ const useStyles = makeStyles((theme: Theme) => ({
   root: {
     // border: '1px solid red',
   },
+  marginContent: {
+    textAlign: 'center',
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(3),
+  },
+  marginHeader: {
+    textAlign: 'center',
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(1),
+  },
   marginRight: {
     marginRight: theme.spacing(0.5),
   },
@@ -53,6 +68,11 @@ const useStyles = makeStyles((theme: Theme) => ({
     // border: '1px solid red'
   },
 }));
+
+interface AlarmConfiguration {
+  label: string;
+  stateKey: string;
+}
 
 export const HeaderClock = (): JSX.Element => {
   const classes = useStyles();
@@ -92,6 +112,23 @@ export const PowerIndicator = (): JSX.Element => {
   );
 };
 
+const alarmConfiguration = (ventilationMode: VentilationMode | null): Array<AlarmConfiguration> => {
+  switch (ventilationMode) {
+    case VentilationMode.hfnc:
+      return [
+        { label: 'SpO2', stateKey: 'spo2' },
+        { label: 'HR', stateKey: 'hr' },
+      ];
+    case VentilationMode.pc_ac:
+    case VentilationMode.vc_ac:
+    case VentilationMode.niv_pc:
+    case VentilationMode.niv_ps:
+    case VentilationMode.psv:
+    default:
+      return [];
+  }
+};
+
 /**
  * ToolBar
  *
@@ -113,13 +150,26 @@ export const ToolBar = ({
   const history = useHistory();
   const currentMode = useSelector(getParametersRequestMode);
   const backendInitialized = useSelector(getBackendInitialized);
-  const parameterRequestStandby = useSelector(getParametersRequestStandby, shallowEqual);
-  const alarmLimitsRequestStandby = useSelector(getAlarmLimitsRequestStandby, shallowEqual);
+  const parameterRequestDraft = useSelector(getParametersRequestDraft, shallowEqual);
   const ventilating = useSelector(getParametersIsVentilating);
+  const alarmLimitsRequestDraftSelect = useSelector(getAlarmLimitsRequestDraft);
+  const alarmLimitsRequestSelect = useSelector(getAlarmLimitsRequest);
+  const alarmLimitsRequestUnsaved = useSelector(getAlarmLimitsRequestUnsaved);
+  const alarmLimitsUnsavedKeys = useSelector(getAlarmLimitsUnsavedKeys);
+  const alarmLimitsRequest = (alarmLimitsRequestSelect as unknown) as Record<string, Range>;
+  const alarmLimitsRequestDraft = (alarmLimitsRequestDraftSelect as unknown) as Record<
+    string,
+    Range
+  >;
   const [isVentilatorOn, setIsVentilatorOn] = React.useState(ventilating);
   const [landingLabel, setLandingLabel] = useState('Loading...');
   const [label, setLabel] = useState('Start Ventilation');
   const [isDisabled, setIsDisabled] = useState(false);
+  const [discardOpen, setDiscardOpen] = useState(false);
+  const setAlarmLimitsRequestDraft = (data: Partial<AlarmLimitsRequest>) => {
+    dispatch(commitDraftRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, data));
+  };
+  const alarmConfig = alarmConfiguration(currentMode);
   // const isDisabled = !isVentilatorOn && location.pathname !== QUICKSTART_ROUTE.path;
   const updateVentilationStatus = () => {
     if (!staticStart) {
@@ -136,7 +186,7 @@ export const ToolBar = ({
   };
 
   const initParameterUpdate = useCallback(() => {
-    if (parameterRequestStandby === null || alarmLimitsRequestStandby === null) {
+    if (parameterRequestDraft === null || alarmLimitsRequestDraft === null) {
       return;
     }
 
@@ -148,14 +198,14 @@ export const ToolBar = ({
       case VentilationMode.hfnc:
         dispatch(
           commitRequest<ParametersRequest>(MessageType.ParametersRequest, {
-            fio2: parameterRequestStandby.fio2,
-            flow: parameterRequestStandby.flow,
+            fio2: parameterRequestDraft.fio2,
+            flow: parameterRequestDraft.flow,
           }),
         );
         dispatch(
           commitRequest<AlarmLimitsRequest>(MessageType.AlarmLimitsRequest, {
-            spo2: alarmLimitsRequestStandby.spo2,
-            hr: alarmLimitsRequestStandby.hr,
+            spo2: alarmLimitsRequestDraft.spo2,
+            hr: alarmLimitsRequestDraft.hr,
           }),
         );
         break;
@@ -166,17 +216,17 @@ export const ToolBar = ({
       case VentilationMode.psv:
         dispatch(
           commitRequest<ParametersRequest>(MessageType.ParametersRequest, {
-            peep: parameterRequestStandby.peep,
-            vt: parameterRequestStandby.vt,
-            rr: parameterRequestStandby.rr,
-            fio2: parameterRequestStandby.fio2,
+            peep: parameterRequestDraft.peep,
+            vt: parameterRequestDraft.vt,
+            rr: parameterRequestDraft.rr,
+            fio2: parameterRequestDraft.fio2,
           }),
         );
         break;
       default:
         break;
     }
-  }, [isVentilatorOn, parameterRequestStandby, alarmLimitsRequestStandby, currentMode, dispatch]);
+  }, [isVentilatorOn, parameterRequestDraft, alarmLimitsRequestDraft, currentMode, dispatch]);
 
   useEffect(() => {
     if (backendInitialized) {
@@ -215,6 +265,16 @@ export const ToolBar = ({
       {staticStart ? landingLabel : label}
     </Button>
   );
+
+  const handleOnClick = () => {
+    if (!alarmLimitsRequestUnsaved) {
+      setDiscardOpen(false);
+      history.push(DASHBOARD_ROUTE.path);
+    } else {
+      setDiscardOpen(true);
+    }
+  };
+
   const tools = [<ModesDropdown />];
   if (location.pathname === DASHBOARD_ROUTE.path) {
     tools.push(<ViewDropdown />);
@@ -226,7 +286,7 @@ export const ToolBar = ({
     // );
   } else if (isVentilatorOn && location.pathname !== SCREENSAVER_ROUTE.path) {
     tools.push(
-      <Button component={Link} to={DASHBOARD_ROUTE.path} variant="contained" color="primary">
+      <Button onClick={handleOnClick} variant="contained" color="primary">
         <BackIcon style={{ paddingRight: 8 }} />
         {DASHBOARD_ROUTE.label}
       </Button>,
@@ -235,6 +295,16 @@ export const ToolBar = ({
   if (location.pathname !== '/') {
     tools.push(<EventAlerts label={LOGS_ROUTE.label} />);
   }
+
+  const handleDiscardClose = () => {
+    setDiscardOpen(false);
+  };
+
+  const handleDiscardConfirm = () => {
+    setAlarmLimitsRequestDraft(alarmLimitsRequest);
+    history.push(DASHBOARD_ROUTE.path);
+    setDiscardOpen(false);
+  };
 
   return (
     <AppBar color="transparent" elevation={0} position="static">
@@ -282,6 +352,38 @@ export const ToolBar = ({
           <Grid item>{StartPauseButton}</Grid>
         </Grid>
       </Grid>
+      <ModalPopup
+        withAction={true}
+        label="Set Alarms"
+        open={discardOpen}
+        onClose={handleDiscardClose}
+        onConfirm={handleDiscardConfirm}
+      >
+        <Grid container alignItems="center">
+          <Grid container alignItems="center" justify="center">
+            <Grid container alignItems="center" className={classes.marginHeader}>
+              <Grid item xs>
+                <Typography variant="h4">Keep Previous Values?</Typography>
+              </Grid>
+            </Grid>
+            <Grid item className={classes.marginContent}>
+              {alarmConfig.map((param: AlarmConfiguration) => {
+                if (alarmLimitsRequest !== null && alarmLimitsRequestDraft !== null) {
+                  if (alarmLimitsUnsavedKeys.includes(param.stateKey)) {
+                    return (
+                      <Typography variant="subtitle1">{`Keep ${param.label} alarm range to ${
+                        alarmLimitsRequest[param.stateKey].lower
+                      } -
+                                ${alarmLimitsRequest[param.stateKey].upper}?`}</Typography>
+                    );
+                  }
+                }
+                return <React.Fragment />;
+              })}
+            </Grid>
+          </Grid>
+        </Grid>
+      </ModalPopup>
     </AppBar>
   );
 };

commit a91f7c3cadb97ebe258713f039c9f99286c7fb02

frontend: Update HFNC control values
* fix bug introduced in #338

frontend/src/modules/displays/MultiStepWizard.tsx
@@ -4,15 +4,14 @@ import { useDispatch } from 'react-redux';
 import { makeStyles, Theme, Grid, Tabs, Tab, Button, Typography } from '@material-ui/core';
 import ReplyIcon from '@material-ui/icons/Reply';
 // import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
-import { useLocation } from 'react-router-dom';
 import ModalPopup from '../controllers/ModalPopup';
 import { getcurrentStateKey, getMultiPopupOpenState, setMultiPopupOpen } from '../app/Service';
 import {
-  getParametersFiO2,
-  getParametersFlow,
   getSmoothedSpO2,
   getSmoothedHR,
   roundValue,
+  getParametersRequestDraftFlow,
+  getParametersRequestDraftFiO2,
 } from '../../store/controller/selectors';
 import { SetValueContent } from '../controllers/ValueModal';
 import { a11yProps, TabPanel } from '../controllers/TabPanel';
@@ -23,7 +22,6 @@ import { ParametersRequest, AlarmLimitsRequest } from '../../store/controller/pr
 import { MessageType } from '../../store/controller/types';
 import { commitRequest, commitDraftRequest } from '../../store/controller/actions';
 import store from '../../store';
-import { DASHBOARD_ROUTE } from '../navigation/constants';
 
 interface Data {
   stateKey: string;
@@ -136,7 +134,7 @@ const HFNCControls = (): JSX.Element => {
       <Grid container item justify="center" alignItems="stretch" direction="column">
         <ValueInfo
           mainContainer={{
-            selector: getParametersFiO2,
+            selector: getParametersRequestDraftFiO2,
             label: 'FiO2',
             stateKey: 'fio2',
             units: PERCENT,
@@ -144,7 +142,7 @@ const HFNCControls = (): JSX.Element => {
         />
         <ValueInfo
           mainContainer={{
-            selector: getParametersFlow,
+            selector: getParametersRequestDraftFlow,
             label: 'Flow',
             stateKey: 'flow',
             units: LMIN,
@@ -282,7 +280,6 @@ const determineInput = (stateKey: string): Data | undefined => {
 const MultiStepWizard = (): JSX.Element => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const location = useLocation();
   const [open, setOpen] = React.useState(false);
   const [confirmOpen, setConfirmOpen] = React.useState(false);
   const [cancelOpen, setCancelOpen] = React.useState(false);
@@ -353,6 +350,10 @@ const MultiStepWizard = (): JSX.Element => {
       const param = multiParams.find((param: Data) => param.stateKey === parameter.stateKey);
       if (param) param.setValue = setting;
       parameter.setValue = setting;
+      if (open) {
+        const update = { [stateKey]: setting };
+        dispatch(commitDraftRequest<ParametersRequest>(MessageType.ParametersRequest, update));
+      }
       if (isAnyChanges()) {
         setIsSubmitDisabled(false);
       } else {
@@ -369,7 +370,7 @@ const MultiStepWizard = (): JSX.Element => {
       // we want to dispatch commitDraftRequest to AlarmLimitsRequest to show the unsaved alarm limit changes
       // in the HFNC control (value info right corner), but doing so when in set alarms page will discard the unsaved changes
       // which is unwanted, thus only do this when we are on dashboard.
-      if (location.pathname === DASHBOARD_ROUTE.path) {
+      if (open) {
         const update = {
           [parameter.stateKey]: {
             lower: min,