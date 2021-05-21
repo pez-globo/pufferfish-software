e258a0d7c740fbbcb503b4904eb239134f42cde8
rohan <purohit.rohan@gmail.com>
2021-05-21 18:56:31 +0530

    backend: improve the backend simulator code

--- backend/ventserver/simulation/alarm_mute.py
+++ backend/ventserver/simulation/alarm_mute.py
@@ -10,81 +10,26 @@ import betterproto
 from ventserver.protocols.backend import states
 from ventserver.protocols.protobuf import mcu_pb
 
-
-def transform_mute(
+def transform(
     request: mcu_pb.AlarmMuteRequest,
     response: mcu_pb.AlarmMute
 ) -> None:
-    """Implement alarm mute service"""
+    """Mute the current active alarm"""
 
     response.active = request.active
 
 @attr.s
-class Service:
-    """Base class for a Alarm Mute simulator."""
+class AlarmMuteService:
+    """Implement Alarm Mute Service"""
 
-    def transform(
-        self, request: mcu_pb.AlarmMuteRequest,
-        response: mcu_pb.AlarmMute
-    ) -> None:
-        """Mute the current active alarm"""
-
-@attr.s
-class PCAC(Service):
-    """Alarm Mute simulator in PC-AC mode."""
-
-    def transform(
-        self, request: mcu_pb.AlarmMuteRequest,
-        response: mcu_pb.AlarmMute
-    ) -> None:
-        """Mute the current active alarm"""
-
-        transform_mute(request, response)
-
-@attr.s
-class HFNC(Service):
-    """Alarm Mute simulator in HFNC mode."""
-
-    def transform(
-        self, request: mcu_pb.AlarmMuteRequest,
-        response: mcu_pb.AlarmMute
-    ) -> None:
-        """Mute the current active alarm"""
-
-        transform_mute(request, response)
-
-@attr.s
-class Services:
-    """Aggregator and selector of services for different modes."""
-
-    _active_service: Optional[Service] = attr.ib(default=None)
-    _services: Dict[mcu_pb.VentilationMode, Service] = attr.ib()
     active_alarm_ids: Dict[mcu_pb.LogEventCode, int] = attr.ib(default={})
-
-    @_services.default    # pylint: disable=R0801
-    def init_services(self) -> Dict[
-            mcu_pb.VentilationMode, Service
-    ]:  # pylint: disable=no-self-use
-        """Initialize the services."""
-        return {
-            mcu_pb.VentilationMode.pc_ac: PCAC(),
-            mcu_pb.VentilationMode.hfnc: HFNC()
-        }
 
     def transform(
             self, store: Mapping[
                 states.StateSegment, Optional[betterproto.Message]
             ]
     ) -> None:
-        """Update the parameters for the requested mode."""
-        parameters = typing.cast(
-            mcu_pb.Parameters, store[states.StateSegment.PARAMETERS]
-        )
-        self._active_service = self._services.get(parameters.mode, None)
-
-        if self._active_service is None:
-            return
-
+        """Update the parameters for alarm mute service"""
         if self.active_alarm_ids is None:
             return
 
@@ -95,6 +40,6 @@ class Services:
         alarm_mute = typing.cast(
             mcu_pb.AlarmMute, store[states.StateSegment.ALARM_MUTE]
         )
-        self._active_service.transform(
+        transform(
             alarm_mute_request, alarm_mute
         )
--- backend/ventserver/simulator.py
+++ backend/ventserver/simulator.py
@@ -121,7 +121,7 @@ async def simulate_states(
     """Simulate evolution of all states."""
     simulation_services = simulators.Services()
     alarms_services = sim_alarms.Services()
-    alarm_mute_service = alarm_mute.Services()
+    alarm_mute_service = alarm_mute.AlarmMuteService()
     active_log_events = typing.cast(
         mcu_pb.ActiveLogEvents,
         store[states.StateSegment.ACTIVE_LOG_EVENTS_MCU]
