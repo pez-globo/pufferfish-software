01948ce158708a947e728d6872fd7adcc3fbf353
rohan <purohit.rohan@gmail.com>
2021-05-31 22:16:59 +0530

    firmware: update power management simulator

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/Power/Simulator.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/Power/Simulator.h
@@ -10,17 +10,22 @@
 #pragma once
 
 #include "Pufferfish/Application/States.h"
+#include "Pufferfish/Driver/I2C/LTC4015/Sensor.h"
 
 namespace Pufferfish::Driver::Power {
 
 class Simulator {
  public:
+  explicit Simulator(Driver::I2C::LTC4015::Sensor &sensor) : ltc4015_sensor_(sensor) {}
+
   void transform(PowerManagement &power_management);
   void transform_charge(PowerManagement &power_management) const;
   void transform_discharge(PowerManagement &power_management) const;
 
  private:
   const float power_responsiveness = 0.5;
+  static const int max_charge = 100;
+  Driver::I2C::LTC4015::Sensor ltc4015_sensor_;
 };
 
 }  // namespace Pufferfish::Driver::Power
--- firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/Power/Simulator.cpp
+++ firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/Power/Simulator.cpp
@@ -14,33 +14,41 @@
 
 namespace Pufferfish::Driver::Power {
 
+// NOLINTNEXTLINE(cert-msc51-cpp)
 std::ranlux24_base prng;
 std::uniform_int_distribution<int> uniform_int(1, 100);
 static constexpr float uniform_width = 1;
-std::uniform_real_distribution<float> uniform(0, uniform_width);
 std::uniform_real_distribution<float> uniform_centered(-uniform_width / 2, uniform_width / 2);
 
 void Simulator::transform(PowerManagement &power_management) {
-  if (!power_management.charging) {
+  InitializableState status = ltc4015_sensor_.output(power_management);
+  if (status != InitializableState::ok) {
     transform_charge(power_management);
-  } else {
-    transform_discharge(power_management);
   }
 }
 
 void Simulator::transform_charge(PowerManagement &power_management) const {
   power_management.power_left +=
       (1 + (power_responsiveness * uniform_centered(prng))) / uniform_int(prng);
-  if (power_management.power_left == 100) {
+  power_management.charging = true;
+  if (power_management.power_left > max_charge) {
+    power_management.power_left = max_charge;
+  }
+  if (power_management.power_left == max_charge) {
     power_management.charging = false;
   }
 }
-void Simulator::transform_discharge(PowerManagement &power_management) const {
-  power_management.power_left -=
-      (1 + (power_responsiveness * uniform_centered(prng))) / uniform_int(prng);
-  if (power_management.power_left == 0) {
-    power_management.charging = true;
-  }
-}
+
+// void Simulator::transform_discharge(PowerManagement &power_management) const {
+//   power_management.power_left -=
+//       (1 + (power_responsiveness * uniform_centered(prng))) / uniform_int(prng);
+
+//   if (power_management.power_left < 0) {
+//     power_management.power_left = 0;
+//   }
+//   if (power_management.power_left == 0) {
+//     power_management.charging = false;
+//   }
+// }
 
 }  // namespace Pufferfish::Driver::Power
