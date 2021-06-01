d681c9e006b47a0070ce64bb3862b333e2900986
rohan <purohit.rohan@gmail.com>
2021-06-01 18:59:16 +0530

    firmware:Sensor.h: remove wait_measurement action and cleanup

--- firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/I2C/LTC4015/Sensor.cpp
+++ firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/I2C/LTC4015/Sensor.cpp
@@ -21,13 +21,6 @@ StateMachine::Action StateMachine::update(uint32_t current_time_us) {
       start_waiting();
       break;
     case Action::measure:
-      next_action_ = Action::wait_measurement;
-      start_waiting();
-      break;
-    case Action::wait_measurement:
-      if (finished_waiting(measuring_duration_us)) {
-        next_action_ = Action::measure;
-      }
       break;
   }
   return next_action_;
@@ -37,10 +30,6 @@ void StateMachine::start_waiting() {
   wait_start_time_us_ = current_time_us_;
 }
 
-bool StateMachine::finished_waiting(uint32_t timeout_us) const {
-  return !Util::within_timeout(wait_start_time_us_, timeout_us, current_time_us_);
-}
-
 // Sensor
 
 InitializableState Sensor::setup() {
@@ -48,7 +37,6 @@ InitializableState Sensor::setup() {
     case Action::initialize:
       return initialize(time_.micros());
     case Action::measure:
-    case Action::wait_measurement:
       return InitializableState::ok;
   }
   return InitializableState::failed;
@@ -58,8 +46,6 @@ InitializableState Sensor::output(PowerManagement &power_management) {
   switch (next_action_) {
     case Action::measure:
       return measure(time_.micros(), power_management);
-    case Action::wait_measurement:
-      next_action_ = fsm_.update(time_.micros());
       return InitializableState::ok;
     default:
       break;
@@ -73,8 +59,9 @@ InitializableState Sensor::initialize(uint32_t current_time_us) {
   }
 
   retry_count_ = 0;
-  // Check if device is ready
-  while (device_.is_device_ready() != I2CDeviceStatus::ok) {
+  // measure
+  bool charging_status = false;
+  while (device_.read_charging_status(charging_status) == I2CDeviceStatus::ok) {
     ++retry_count_;
     if (retry_count_ > max_retries_setup) {
       return InitializableState::failed;
