44dc0154a2495f57f963c82b5d90618305f99159
rohan <purohit.rohan@gmail.com>
2021-06-21 08:05:33 +0530

    firmware: power: add alarms for low battery

--- firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/Power/AlarmsService.h
+++ firmware/ventilator-controller-stm32/Core/Inc/Pufferfish/Driver/Power/AlarmsService.h
@@ -13,13 +13,23 @@
 namespace Pufferfish::Driver::Power {
 
 // All alarm codes for power management need to be registered in the following array:
-static constexpr auto alarm_codes =
-    Util::Containers::make_array<LogEventCode>(LogEventCode::LogEventCode_charger_disconnected);
+static constexpr auto alarm_codes = Util::Containers::make_array<LogEventCode>(
+    LogEventCode::LogEventCode_charger_disconnected,
+    LogEventCode::LogEventCode_battery_low,
+    LogEventCode::LogEventCode_critical_battery);
 
 class AlarmsService {
  public:
-  static void transform(
-      const MCUPowerStatus &mcu_power_status, Application::AlarmsManager &alarms_manager);
+  void transform(
+      const MCUPowerStatus &mcu_power_status, Application::AlarmsManager &alarms_manager) const;
+  static void generate_alarm(
+      const LogEventCode &alarm_code,
+      bool alarm_active,
+      Application::AlarmsManager &alarms_manager);
+
+ private:
+  const float battery_low_power = 30;
+  const float critical_battery_power = 5;
 };
 
 }  // namespace Pufferfish::Driver::Power
--- firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/Power/AlarmsService.cpp
+++ firmware/ventilator-controller-stm32/Core/Src/Pufferfish/Driver/Power/AlarmsService.cpp
@@ -9,15 +9,34 @@
 
 namespace Pufferfish::Driver::Power {
 
+void AlarmsService::generate_alarm(
+    const LogEventCode &alarm_code, bool alarm_active, Application::AlarmsManager &alarms_manager) {
+  if (alarm_active) {
+    alarms_manager.activate_alarm(alarm_code, LogEventType::LogEventType_system);
+  } else {
+    alarms_manager.deactivate_alarm(alarm_code);
+  }
+}
+
 // AlarmsService
 void AlarmsService::transform(
-    const MCUPowerStatus &mcu_power_status, Application::AlarmsManager &alarms_manager) {
+    const MCUPowerStatus &mcu_power_status, Application::AlarmsManager &alarms_manager) const {
+  bool charger_disconnected = false;
   if (!mcu_power_status.charging) {
-    alarms_manager.activate_alarm(
-        LogEventCode_charger_disconnected, LogEventType::LogEventType_system);
-  } else {
-    alarms_manager.deactivate_alarm(LogEventCode_charger_disconnected);
+    charger_disconnected = true;
   }
+  bool battery_low = false;
+  if (mcu_power_status.power_left < battery_low_power) {
+    battery_low = true;
+  }
+  bool critical_battery = false;
+  if (mcu_power_status.power_left <= critical_battery_power) {
+    critical_battery = true;
+  }
+
+  generate_alarm(LogEventCode_charger_disconnected, charger_disconnected, alarms_manager);
+  generate_alarm(LogEventCode_battery_low, battery_low, alarms_manager);
+  generate_alarm(LogEventCode_critical_battery, critical_battery, alarms_manager);
 }
 
 }  // namespace Pufferfish::Driver::Power
