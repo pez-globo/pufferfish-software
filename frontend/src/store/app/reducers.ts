224fcc13a326a8aaa7010c8e15d3ff2558b0e683
rohan <purohit.rohan@gmail.com>
2021-07-10 12:23:53 +0530

    update reducers for backend connection actions

--- frontend/src/store/app/reducers.ts
+++ frontend/src/store/app/reducers.ts
@@ -35,9 +35,9 @@ export function appReducer(state = initialState, action: AppAction): AppState {
       // it should come from the code which creates the action to dispatch.
       return { ...state, backendHeartbeat: new Date() };
     case BACKEND_CONNECTION_DOWN:
-      return { ...state, clock: new Date(), backendConnection: false };
+      return { ...state, backendConnection: false };
     case BACKEND_CONNECTION_UP:
-      return { ...state, clock: new Date(), backendConnection: true };
+      return { ...state, backendConnection: true };
     default:
       return state;
   }
