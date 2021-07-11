6f344b186c5e4db111958d2996fa69b04cc0164b
rohan <purohit.rohan@gmail.com>
2021-07-11 14:30:49 +0530

    fix renaming

--- frontend/src/modules/controllers/ValueController.tsx
+++ frontend/src/modules/controllers/ValueController.tsx
@@ -5,7 +5,7 @@
 import React, { RefObject } from 'react';
 import { Grid, makeStyles, Theme } from '@material-ui/core';
 import { ValueClicker } from '.';
-import ParameterInfo from '../displays/ValueDisplay';
+import ValueDisplay from '../displays/ValueDisplay';
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -67,7 +67,7 @@ const ValueController = ({
   return (
     <Grid ref={reference} container className={classes.root}>
       <Grid item xs>
-        <ParameterInfo value={value} label={label} units={units} />
+        <ValueDisplay value={value} label={label} units={units} />
       </Grid>
       <Grid item>
         <ValueClicker
