f5e883feb595535668bff654681eaf824ef3d4ec
rohan <purohit.rohan@gmail.com>
2021-07-14 09:27:30 +0530

    fix eslint warnings

--- frontend/src/store/controller/connection/backend.ts
+++ frontend/src/store/controller/connection/backend.ts
@@ -1,8 +1,13 @@
-import { delay, put, select } from 'redux-saga/effects';
+import { CallEffect, delay, put, PutEffect, select, SelectEffect } from 'redux-saga/effects';
 import { establishedBackendConnection, lostBackendConnection } from '../../connection/actions';
 import { getBackendConnected } from '../../connection/selectors';
+import { ConnectionAction } from '../../connection/types';
 
-export function* watchBackendHeartbeat() {
+export function* watchBackendHeartbeat(): Generator<
+  SelectEffect | CallEffect<true> | PutEffect<ConnectionAction>,
+  void,
+  unknown
+> {
   while (true) {
     yield delay(3000);
     const backendConnected = yield select(getBackendConnected);
@@ -12,7 +17,11 @@ export function* watchBackendHeartbeat() {
   }
 }
 
-export function* watchBackendConnection() {
+export function* watchBackendConnection(): Generator<
+  SelectEffect | PutEffect<ConnectionAction>,
+  void,
+  unknown
+> {
   while (true) {
     const backendConnected = yield select(getBackendConnected);
     if (!backendConnected) {
