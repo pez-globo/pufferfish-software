0b58db42748a14b2a9c46e1386f09cc784a3e197
rohan <purohit.rohan@gmail.com>
2021-09-12 13:10:27 +0530

    fix imports

--- frontend/src/modules/alarms/modal/AlarmModal.tsx
+++ frontend/src/modules/alarms/modal/AlarmModal.tsx
@@ -12,7 +12,7 @@ import { MessageType } from '../../../store/proto/types';
 import ModalPopup from '../../shared/ModalPopup';
 import ValueClicker from '../../controllers/ValueClicker';
 import ValueSlider from '../../controllers/ValueSlider';
-import useRotaryReference from '../../shared/useRotaryReference';
+import useRotaryReference from '../../shared/rotary/useRotaryReference';
 import { setActiveRotaryReference } from '../../app/Service';
 
 const useStyles = makeStyles((theme: Theme) => ({
--- frontend/src/modules/shared/index.ts
+++ frontend/src/modules/shared/index.ts
@@ -1,4 +1,3 @@
 export * from './ModalPopup';
-export { default as Knob } from './Knob';
 export { default as TabPanel } from './TabPanel';
 export { default as TestTool } from './TestTool';
